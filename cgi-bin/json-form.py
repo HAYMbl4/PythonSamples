5df25638f1ad9a5306278230b0c575f1632203d5
Oleg Naumov <o.naumov@onbrains.org>
2017-06-04 18:56:57 +0300

    Разнёс функционал по фичам

--- cgi-bin/json-form.py
+++ cgi-bin/json-form.py
@@ -10,10 +10,10 @@ wish = form.getfirst("wish", "empty")
 name_for_wish = html.escape(name_for_wish)
 wish = html.escape(wish)
 
-with open("wishes.json", "r") as old_wishes_file:
+with open("wish/wishes.json", "r") as old_wishes_file:
     old_wishes = json.load(old_wishes_file)
 
-with open("wishes.json", "w") as new_wishes_file:
+with open("wish/wishes.json", "w") as new_wishes_file:
     try:
         old_wishes["wishes"].append({"name": name_for_wish, "wish": wish})
         json.dump(old_wishes, new_wishes_file)
--- cgi-bin/will-go.py
+++ cgi-bin/will-go.py
@@ -6,10 +6,10 @@ form = cgi.FieldStorage()
 will_go_array = form.getfirst("will-go-array")
 new_visitors = json.loads(will_go_array)
 
-with open("visitors.json", "r") as old_visitors_file:
+with open("visitor/visitors.json", "r") as old_visitors_file:
     old_visitors = json.load(old_visitors_file)
 
-with open("visitors.json", "w") as new_visitors_file:
+with open("visitor/visitors.json", "w") as new_visitors_file:
     for v in new_visitors:
         old_visitors["visitors"].append(v)
         json.dump(old_visitors, new_visitors_file)
