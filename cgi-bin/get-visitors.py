390b6ba791bde2f95aed55ed93d0bd471a36a34d
Oleg Naumov <o.naumov@onbrains.org>
2017-06-21 06:36:32 +0300

    Удалил ненужные файлы

--- cgi-bin/get-visitors.py
+++
@@ -1,7 +0,0 @@
-import json
-
-with open("visitor/visitors.json", "r") as visitors_file:
-    visitors = json.load(visitors_file)
-
-print('Content-Type: application/json\n\n')
-print(json.dumps(visitors))
